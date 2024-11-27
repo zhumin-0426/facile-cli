d2e578fcd67cd3a49b182a551b6ef6f129ba6584
Darian <971367636@qq.com>
2024-11-27 15:06:11 +0800

    eslint

---
+++ eslint.config.js
@@ -0,0 +1,29 @@
+import eslint from "@eslint/js";
+import globals from "globals";
+
+export default [
+    {
+        ignores: [
+          "packages/test/**/*.js",
+          "**/docs/**",
+          "**/examples/**",
+          "**/template/**"
+        ]
+    },
+    eslint.configs.recommended,
+    {
+        name:"facile",
+        languageOptions:{
+            ecmaVersion:2024,
+            sourceType:"module",
+            globals:{
+                ...globals.node,
+                ...globals.es2024
+            }
+        },
+        rules: {
+            "no-unused-vars": "warn",
+            "no-undef": "warn"
+        }
+    }
+];
