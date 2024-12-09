8bc461aba3bbf7ec9fa7eec627764ab88d59addb
Darian <971367636@qq.com>
2024-12-09 16:53:36 +0800

    init

--- packages/cli/bin/facile.js
+++ packages/cli/bin/facile.js
@@ -1,1 +1,4 @@
-console.log("AAAAA")
+#!/usr/bin/env node
+const yargs = require('yargs');
+console.log('11111name', yargs.argv);
+console.log(require('yargs/yargs')(process.argv.slice(2)).parse().alias)
